d9978ef46df51380738727fe8e769f8ad4db2748
ron <ronnakorn.sop@knights.ucf.edu>
2019-11-12 17:48:59 -0500

    add stuff

--- EventManagerWebApp/Site.Master.designer.cs
+++ EventManagerWebApp/Site.Master.designer.cs
@@ -42,6 +42,15 @@ namespace EventManagerWebApp
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl liStudent;
 
         /// <summary>
+        /// liAdmin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liAdmin;
+
+        /// <summary>
         /// liSuperAdmin control.
         /// </summary>
         /// <remarks>
